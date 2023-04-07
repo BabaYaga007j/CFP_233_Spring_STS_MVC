5af8a9b1b057f5b7e875d7856c28d3f7c32e8586
BabaYaga007j <soumyasourav35@gmail.com>
2023-04-07 20:47:56 +0530

    Hello World Spring App

---
+++ src/main/java/com/example/springMVC/contoller/MyController.java
@@ -0,0 +1,15 @@
+package com.example.springMVC.contoller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+    @GetMapping("/web")
+    public String greeting(){
+
+        return "hello";
+    }
+
+}
